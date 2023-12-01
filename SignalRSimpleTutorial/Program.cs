622b9a117dd3d6514eed80f2adb0f77492dc0efa
Joao <joao.c@outlook.com>
2023-12-01 09:26:57 -0300

    remove useless connection dependency injection

--- SignalRSimpleTutorial/Program.cs
+++ SignalRSimpleTutorial/Program.cs
@@ -1,5 +1,3 @@
-using System.Threading;
-using Microsoft.AspNetCore.SignalR.Client;
 using SignalRSimpleTutorial;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,14 +6,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
-builder.Services.AddScoped(sp=>
-{
-    var conn = new HubConnectionBuilder()
-        .WithUrl("https://localhost:7238/chat")
-        .Build();
-    conn.StartAsync().GetAwaiter().GetResult();
-    return conn;
-});
 
 var app = builder.Build();
 app.MapHub<MainHub>("chat");
